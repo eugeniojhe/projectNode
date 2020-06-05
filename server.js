b146b39074af45e408f9e7bb81611192e8318489
eugenio <eugeniojhe@gmail.com>
2020-06-05 18:24:14 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config({path:'variables.env'}); 
+mongoose.connect(process.env.DATABASE); 
+mongoose.Promise = global.Promise; 
+mongoose.connection.on('error',(error)=>{
+    console.error('error Connection '+error.message); 
+});
+ 
+
+app.set('port',process.env.PORT || 7777); 
+//app.set('port',7777);
+const server = app.listen(app.get('port'),()=>{
+    console.log('Hello Server running '+server.address().port); 
+});
